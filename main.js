59dfe965a1ae6e2dfc4ee013921b6c8e4781306b
lavanyaananth <lavanyaanand.11@gmail.com>
2019-08-08 23:11:17 +0530

    sample fcc api check

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log("hey test");
-console.log("my key is key=AIzaSyBXuhdA2pvUROqskcOczHLyARdFjQ58VzU");
+
 
 /* getting lat and lat */
 navigator.geolocation.getCurrentPosition(function(position){
@@ -9,19 +9,7 @@ navigator.geolocation.getCurrentPosition(function(position){
     
     document.getElementById("lat").innerHTML = latitude;
     document.getElementById("lon").innerHTML = longitude;
-    /* getting location from Google */
-    fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng='  + latitude + ',' + longitude +'& result_type=postal_codes&key=AIzaSyBXuhdA2pvUROqskcOczHLyARdFjQ58VzU')
-    .then(response => response.json())
-    .then(data => console.log((data)))
-
-    const request = async () => {
-        const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng='  + latitude + ',' + longitude +'& result_type=postal_codes&key=AIzaSyBXuhdA2pvUROqskcOczHLyARdFjQ58VzU');
-        const json = await response.json();
-        console.log(json.results[4].formatted_address);
-        var loc = json.results[4].formatted_address;
-        document.getElementById("place").innerHTML = loc;
-    }
-    request();
+   
 
     /* Getting temperature from free code camp API */
     fetch('https://fcc-weather-api.glitch.me/api/current?lat='+ latitude + '&lon=' +longitude)
