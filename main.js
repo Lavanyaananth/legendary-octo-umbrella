49bf1b5337b1aee2c62c647edd0b66a84e7f7d5c
lavanyaananth <lavanyaanand.11@gmail.com>
2019-08-15 02:13:41 +0530

    adding comments and screenshots

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 /* getting lat and lat */
 navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position);
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    
         
     /* getting weather details from API */
     const weatherreq = async () => {
         const response = await fetch('https://fcc-weather-api.glitch.me/api/current?lat='+ latitude + '&lon=' +longitude);
         const json = await response.json();
-        console.log(json);
-
-        document.getElementById('foo').onclick = function(){
-        
-        }
-
+    
+    /* Binding values - Weather icon, type of the day, location, temperature*/
         document.getElementById("weather_icon").src = json.weather[0].icon;
         var day_type = json.weather[0].description;
         document.getElementById("day").innerHTML = day_type ;
         var loc = json.name;
         document.getElementById("place").innerHTML = loc;
+
+    /* Temperature calculation in celcius and  fahrenheit */
         var temp = Math.round(json.main.temp);
         document.getElementById("temperaturec").innerHTML = temp + ' '+ '&#x2103'  ;
         console.log("testing conversion");
         document.getElementById("temperaturef").innerHTML = Math.round(temp * 9/5 +32) +  ' '+ '&#x2109'
        
+    /* Toggling */
         document.getElementById("foo").onclick = function(){
             var x = document.getElementById("temperaturef");
             var y = document.getElementById("temperaturec");
-            console.log(x);
             if (window.getComputedStyle(x, null).getPropertyValue("display") === 'none') {
                 x.style.display = "block";
                 y.style.display = "none";
